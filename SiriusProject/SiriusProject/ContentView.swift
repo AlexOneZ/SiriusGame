2a559fd99acc516ed5e44b9ed628adbe7fa57ca6
AlexOneZ <kobiale92@gmail.com>
2025-03-25 16:43:27 +0300

    init

---
+++ SiriusProject/SiriusProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SiriusProject
+//
+//  Created by Алексей Кобяков on 25.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
