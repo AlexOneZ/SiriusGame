faa8b7966ea8f88cf602afa2180122ac52dbb598
Ramin Sultangaliev <raminsultan07@mail.ru>
2025-03-26 19:28:33 +0300

    task-47/1: добавлены get_teams,add_team

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from database import create_tables, delete_tables
-from router import router as events_router
+
+from events.router import router as events_router
+from teams.router import router as teams_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,4 +15,6 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
 app.include_router(events_router)
+app.include_router(teams_router)
