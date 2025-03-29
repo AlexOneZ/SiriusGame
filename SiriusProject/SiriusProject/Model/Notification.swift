500fae311beeaa9c0ddb2e72fde7ecefd03a1fa8
mary_ss <mayorowamasha5@gmail.com>
2025-03-29 15:27:11 +0300

    task-73/1: add notifications screen

---
+++ SiriusProject/SiriusProject/Model/Notification.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationModel.swift
+//  SiriusProject
+//
+//  Created by Maria Mayorova on 28.03.2025.
+//
+
+import SwiftUI
+
+struct Notification {
+    let title: String
+    let body: String
+}
+
+struct MockData {
+    static let sampleNotification = Notification(title: "Title",
+                                                 body: "This is a long long long text for myb test notofication to check what will be done if the message will be too long")
+}
