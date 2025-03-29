63c0e9a76cc67d7f9f1dd0f927c8c2616284fcfd
mary_ss <mayorowamasha5@gmail.com>
2025-03-29 09:42:21 +0300

    task-74/1: swiftformat change

--- SiriusProject/SiriusProject/Managers/NotificationsManager.swift
+++ SiriusProject/SiriusProject/Managers/NotificationsManager.swift
@@ -10,11 +10,10 @@ import UIKit
 import UserNotifications
 
 class CustomAppDelegate: UIResponder, UIApplicationDelegate {
-    
     var app: SiriusProjectApp?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted,  error in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
             if granted {
                 DispatchQueue.main.async {
                     UIApplication.shared.registerForRemoteNotifications()
@@ -24,22 +23,28 @@ class CustomAppDelegate: UIResponder, UIApplicationDelegate {
         UNUserNotificationCenter.current().delegate = self
         return true
     }
-    
-    func application(_ application: UIApplication,
-                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
         let stringifiedToken = deviceToken.map { String(format: "%02hhx", $0) }.joined()
         print("stringifiedToken:", stringifiedToken)
     }
 }
 
 extension CustomAppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse) async {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse
+    ) async {
         // go to the screen with notifications
     }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification
+    ) async -> UNNotificationPresentationOptions {
         return [.badge, .banner, .list, .sound]
     }
 }
