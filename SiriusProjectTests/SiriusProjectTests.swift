77a58ad059f926f857ac2391f1ed8e5f057bfc37
mary_ss <mayorowamasha5@gmail.com>
2025-04-03 23:05:42 +0300

    task-137/1: add unit tests scheme in xcodegen

---
+++ SiriusProjectTests/SiriusProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  SiriusProjectTests.swift
+//  SiriusProjectTests
+//
+//  Created by Maria Mayorova on 03.04.2025.
+//
+
+import Testing
+
+struct SiriusProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
